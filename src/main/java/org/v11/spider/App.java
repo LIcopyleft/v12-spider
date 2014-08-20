1452dec7cad3d461c5ad0ea65e8d8f6dbb781e2b
v11 <442629928@qq.com>
2014-08-20 17:48:13 +0800

    创建项目
    新建maven项目

---
+++ src/main/java/org/v11/spider/App.java
@@ -0,0 +1,13 @@
+package org.v11.spider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
